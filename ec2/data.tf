6416b3fa364fd9aae5fb56974963f8d180b215fd
s-shodiyona <160341432+s-shodiyona@users.noreply.github.com>
2024-05-01 18:51:07 -0400

    added output

--- ec2/data.tf
+++ ec2/data.tf
@@ -11,7 +11,7 @@ data "terraform_remote_state" "vpc" {
 
 data "aws_ami" "ubuntu" {
   most_recent = true
-  
+
   filter {
     name   = "name"
     values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-*"]
